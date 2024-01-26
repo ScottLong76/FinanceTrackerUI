a02a171c672350331ea69df012fe0d985437dd4f
ScottLong76 <ScottLong76@gmail.com>
2024-01-26 00:10:25 -0500

    Early wireframe of the app

---
+++ src/scenes/global/Topbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Topbar: React.FC = () => {
+    return (
+        <div className="topbar">
+            Welcome to the topbar!
+            {/* Add your menu items here */}
+        </div>
+    );
+};
+
+export default Topbar;
